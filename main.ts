28d2d74e861937cb85291570db9494a1b7d2bbd4
Josjeroobol <124696948+Josjeroobol@users.noreply.github.com>
2023-04-24 13:16:11 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 25)
     basic.pause(1000)
     maqueen.motorStop(maqueen.Motors.All)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
-    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 25)
+    basic.pause(1525)
 })
